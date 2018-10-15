0b148355885494834678daaf8144286d7680e3ff
surya.k.kumaresan <surya.k.kumaresan@BDc8-Dx-6PHHH92.dir.svc.accenture.com>
2018-10-15 17:12:11 +0530

    done

--- DatabaseTest/src/test/java/SQL_DB_TEST/personalSqlDB.java
+++ DatabaseTest/src/test/java/SQL_DB_TEST/personalSqlDB.java
@@ -6,18 +6,19 @@ import databaseImplements.dbCommonMethods;
 public class personalSqlDB {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		runSqlQuery("INSERT INTO STATION VALUES (32, 'ganeshan', 'AZ', 33, 112); ", "city");
-		runSqlQuery("select * from station;", "City");
+		runSqlQuery("create table professional ( userId int, name varchar(50))", "");
+		runSqlQuery("select * from student;", "Name");
 	}
 
 	public static void runSqlQuery(String sqlQuery, String colmName) {
 		try {
 			String[] dbDataList = null;
 			dbCommonMethods dbObj = new dbCommonMethods();
-			dbObj.getSqlConnection("RAY-PC", "test01");
+			dbObj.getSqlConnection("BDC8-DX-6PHHH92\\SQLEXPRESS", "Profiles");
 			dbDataList = dbObj.runQuery(sqlQuery, colmName);
-			for (String output : dbDataList)
-				System.out.println(output);
+			if (dbDataList != null)
+				for (String output : dbDataList)
+					System.out.println(output);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
